56fcc812cb68980cc979cd5ebf88a6a331733c3a
DBosDavinci <99069008@mydavinci.nl>
2021-09-17 15:26:47 +0200

    dagenvdweek.py

---
+++ duizelig/dagenvdweek.py
@@ -0,0 +1,6 @@
+dagen = "ma", "di", "wo", "do", "vr", "za", "zo"
+vraag = input("Welke dag van de week kiest u? {} ".format(dagen))
+i = 0
+while i < dagen.index(vraag)+1:
+    print(dagen[i])
+    i+=1

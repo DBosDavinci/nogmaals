030a3b4b18c3636c9c5317b8d066c98d08fc0913
BuildTools <unconfigured@null.spigotmc.org>
2021-09-20 15:30:04 +0200

    example12.py

--- robotarm-python-2021-main/example12.py
+++ robotarm-python-2021-main/example12.py
@@ -1,10 +1,24 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 12')
 
 # Jouw python instructies zet je vanaf hier:
 
+move = 9
 
+for x in range(9):
+    robotArm.grab()
+    color = robotArm.scan()
+    if color == "red":
+        for x in range(move):
+            robotArm.moveRight()
+        robotArm.drop()
+        for x in range((move-1)):
+            robotArm.moveLeft()
+    else:
+        robotArm.drop()
+        robotArm.moveRight()
+    move-=1
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()

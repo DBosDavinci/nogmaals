aca01824d433c63f31f0efcdd275aeb03443407e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-20 14:49:35 +0200

    example9.py

---
+++ robotarm-python-2021-main/example4.py
@@ -0,0 +1,29 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 4')
+
+# Jouw python instructies zet je vanaf hier:
+
+robotArm.grab()
+for x in range(1,3):
+    robotArm.moveRight()
+robotArm.drop()
+for x in range(1,3):
+    robotArm.moveLeft()
+robotArm.grab()
+for x in range(1,3):
+    robotArm.moveRight()
+robotArm.drop()
+for x in range(1,3):
+    robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+for x in range(1,3):
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
